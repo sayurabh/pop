0f12d54637c2ce5681031803bf1e76803b1cfa89
sayurabh <sayurabh@gmail.com>
2018-05-09 22:10:49 +0530

    adding all the files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"database":"mongodb://root:27341895@ds111535.mlab.com:11535/ecommerce",
+	"port":process.env.PORT || 3000,
+	"secretKey":"YourSecretKey"
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+mongoose.connect(config.database,function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log('Connected to the databse');
+	}
+});
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+var api = require('./app/routes/api.js')(app, express, io);
+app.use('/api',api);
+app.get('*',function(req,res){
+	res.sendFile(__dirname + '/public/app/views/index.html');
+});
+http.listen(config.port,function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log("Listning on port 3000");
+	}
+});
